258f213cda13a28fb07ff1ab1cff0ecd49164f10
hulyakucuk <0hulyakucuk@gmail.com>
2023-05-25 19:15:50 +0300

    add animation for links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,10 +6,17 @@ import { NavLink } from 'react-router-dom';
 export default function Navbar() {
 
     const navRef=useRef();
-
-    const showNav=()=>{
-        navRef.current.classList.toggle("navbar");
-    }
+    const showNav = () => {
+      navRef.current.classList.toggle("navbar");
+    
+      Array.from(navRef.current.children).forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `linksFade 0.5s ease-in forwards ${index / 5 }s`;
+        }
+      });
+    };
     
   return (
     <header>
@@ -24,7 +31,7 @@ export default function Navbar() {
             <NavLink to='/register'>Register</NavLink>
         <button className='nav-btn nav-close-btn' onClick={showNav}><FaTimes/></button>
         </nav>
-        <button className='nav-btn' onClick={showNav}><FaBars/></button>
+        <button className='nav-btn' onClick={showNav} ><FaBars/></button>
     </header>
   )
 }
