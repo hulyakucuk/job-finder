77dd942a198d7b49a8c085ade2eb8ed26448a888
hulyakucuk <0hulyakucuk@gmail.com>
2023-05-25 07:54:11 +0300

    create components directory and add navbar.js

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+        <h3>Logo</h3>
+        <nav>
+            <a href='#'>Jobs</a>
+            <a href='#'>Companies</a>
+            <a href='#'>About</a>
+            <a href='#'>Contact</a>
+            <a href='#'>Blog</a>
+            <a href='#'>Login</a>
+            <a href='#'>Register</a>
+
+        </nav>
+    </header>
+  )
+}
