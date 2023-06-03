5b89aeaf034ca7ee740a70f234c657bd608f6206
hulyakucuk <0hulyakucuk@gmail.com>
2023-06-03 18:01:05 +0300

    add filter template to search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -20,7 +20,40 @@ export default function Search() {
           <div className="search-btn">
             <button>Search</button>
           </div>
+
         </div>
+        <div className="section-div">
+            <div className="filter-div">
+            <label htmlFor="sort">Sort by:</label>
+            <select name="" id="sort">
+              <option value="">Relevance</option>
+              <option value="">Inclusive</option>
+              <option value="">Starts with</option>
+              <option value="">Contains</option>
+            </select>
+            </div>
+       <div className="filter-div">
+       <label htmlFor="type">Type:</label>
+       <select id="type" name="">
+              <option value="full-time">Full-time</option>
+              <option value="full-time">Remote</option>
+              <option value="full-time">Contract</option>
+              <option value="full-time">Part-time</option>
+            </select>
+       </div>
+       <div className="filter-div" >
+        <label htmlFor="level" >Level:</label>
+        <select id="level">
+          <option value="">Senior</option>
+          <option value="">Begginner</option>
+          <option value="">Intermediate</option>
+          <option value="">Adocate</option>
+        </select>
+       </div>
+       <div className="clear-all-btn filter-div">
+            <button>Clear-all</button>
+          </div>
+          </div>
       </div>
     </>
   );
